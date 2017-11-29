cfdd5c41408dd39a8e6b7a26a4626b8651865a2f
hashinfoways <34103520+hashinfoways@users.noreply.github.com>
2017-11-29 22:45:19 +0530

    Add files via upload

---
+++ LICENSE.md/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		items:1,
+		lazyLoad:true,
+		loop:true,
+		animateIn: 'fadeIn',
+		animateOut: 'fadeOut',
+		autoplay: true
+	});
+});
